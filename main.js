e2b319393e2b4426c36f3462ae9100a6612915ce
Jason Xie <jason.xie@victheme.com>
2019-07-09 07:57:25 +0800

    bug fix

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron');
 
-process.env.JX_ENV = 'development';
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
